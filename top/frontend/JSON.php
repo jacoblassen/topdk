7470a8bd22cb667a0985af1750164577391724dd
Jacob <jabberl5@hotmail.com>
2014-12-09 11:44:30 +0100

    JSON HTML form added with simpe stylesheet and XML form page created

---
+++ top/frontend/JSON.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>JSON</title>
+  <link rel="stylesheet" href="stylesheet.css" />
+</head>
+  <body>
+    <div id="master">
+      <h1>JSON</h1>
+      <form action="#" method="POST">
+        <div>
+          <label for="name">Navn</label>
+          <input type="text" name="name" id="name" />
+        </div>
+        <div>
+          <label for="cpr">CPR-/kundenummer</label>
+          <input type="text" name="cpr" id="cpr" />
+        </div>
+        <div>
+          <label for="email">E-mail</label>
+          <input type="text" name="email" id="email" />
+        </div>
+        <div>
+          <label for="tlf">Telefon</label>
+          <input type="text" name="tlf" id="tlf" />
+        </div>
+        <div>
+          <label for="tlfTime">Bedste træffetid</label>
+          <input type="text" name="tlfTime" id="tlfTime" />
+        </div>
+        <div>
+          <label for="accidentDate">Dato for ulykke</label>
+          <input type="text" name="accidentDate" id="accidentDate" />
+        </div>
+        <div>
+          <label for="how">Hvordan</label>
+          <input type="text" name="how" id="how" />
+        </div>
+        <div>
+          <label for="injured">Antal tilskadekomne</label>
+          <input type="text" name="injured" id="injured" />
+        </div>
+        <div>
+          <input type="hidden" name="flowType" value="JSON" />
+        </div>
+        <div>
+          <input type="submit">
+        </div>
+      </form>
+    </div>
+  </body>
+</html>
