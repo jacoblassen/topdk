01f9b8609f633d257b834f5388f4de43a142ee91
Jacob <jabberl5@hotmail.com>
2014-12-09 11:08:38 +0100

    added links to frontend index

--- top/frontend/index.php
+++ top/frontend/index.php
@@ -1,1 +1,23 @@
-<p>branch</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>XML/JSON</title>
+</head>
+  <body>
+    <div id="master">
+      <div id="wrapper">
+        <a href="JSON.php">
+          <div class="button">
+            <h1>XML</h1>
+          </div>
+        </a>
+        <a href="XML.php">
+          <div class="button">
+            <h1>JSON</h1>
+          </div>
+        </a>
+      </div>
+    </div>
+  </body>
+</html>
