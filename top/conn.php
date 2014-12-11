9089cfc2167180fa62a675d95e1dc3f71f3fa644
Jacob <jabberl5@hotmail.com>
2014-12-11 12:59:37 +0100

    conn.php now contains stuff

--- top/conn.php
+++ top/conn.php
@@ -1,2 +1,16 @@
 <?php
+  $server = "127.0.0.1";
+  $user = "root";
+  $password = "root";
+  $database = "lam";
+  $port = "3306";
+
+  //Create connection
+  $conn = new mysqli($server, $user, $password, $database, $port);
+
+  //Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully";
 ?>
