cf097e5158ec62e27e0edeecfa38e66e00baa7bf
Jacob <jabberl5@hotmail.com>
2014-12-16 19:46:44 +0100

    json schema validation seem to be working

--- liv/conn.php
+++ liv/conn.php
@@ -12,7 +12,7 @@
   if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
   }
-  echo "Connected successfully";
+  //echo "Connected successfully";
   /* Kode taget "fra http://php.net/manual/en/mysqli.set-charset.php" */
   if (!$mysqli->set_charset("utf8")) {
     printf("Error loading character set utf8: %s\n", $mysqli->error);
--- liv/decodeJSON.php
+++ liv/decodeJSON.php
@@ -1,10 +1,14 @@
 <?php
   require_once 'conn.php';
   require_once 'jsv4-php/jsv4.php';
-  echo '1';
+
   $data = file_get_contents('schemas/testString.json');
   $schema = file_get_contents('schemas/JSON.json');
-  $validate = Jsv4::isValid(json_decode(json_encode($data)), json_decode(json_encode($schema)));
+
+  $data = json_decode($data);
+  $schema = json_decode($schema);
+
+  $validate = Jsv4::isValid($data, $schema);
+
   var_dump($validate);
-  //echo $data;
 ?>
