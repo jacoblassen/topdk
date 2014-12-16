4a9c67f7f54694f3f73a049fedf1c64d9ccd556f
Jacob <jabberl5@hotmail.com>
2014-12-16 12:37:25 +0100

    uncommented utf-8 in conn.php

--- top/conn.php
+++ top/conn.php
@@ -14,10 +14,7 @@
   }
   /*echo "Connected successfully";
   /* Kode taget "fra http://php.net/manual/en/mysqli.set-charset.php" */
-  /*if (!$mysqli->set_charset("utf8")) {
+  if (!$mysqli->set_charset("utf8")) {
     printf("Error loading character set utf8: %s\n", $mysqli->error);
-  } else {
-    printf("Current character set: %s\n", $mysqli->character_set_name());
   }
-  echo '<br />'*/
 ?>
