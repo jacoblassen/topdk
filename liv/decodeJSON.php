1f6d5d301d9e167c3e6afdd1425672516c07d905
Jacob <jabberl5@hotmail.com>
2014-12-17 10:20:43 +0100

    json schema is now pretty

--- liv/decodeJSON.php
+++ liv/decodeJSON.php
@@ -2,7 +2,7 @@
   require_once 'conn.php';
   require_once 'jsv4-php/jsv4.php';
 
-  $data = file_get_contents('schemas/testString.json');
+  $data = file_get_contents('http://192.168.10.10/fetchJSON.php');
   $schema = file_get_contents('schemas/JSON.json');
 
   $data = json_decode($data);
